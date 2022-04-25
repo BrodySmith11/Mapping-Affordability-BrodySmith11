f745c173d2bf90129363d2a49631a8910ec91227
BrodySmith11 <98329937+BrodySmith11@users.noreply.github.com>
2022-04-24 21:31:20 -0400

    Day 3 Update
    Aaron assisted

--- affordability.py
+++ affordability.py
@@ -7,8 +7,20 @@ Created on Wed Apr 20 08:06:43 2022
 
 import pandas as pd
 
-income = int(input('Enter Yearly Income:'))
+income = int(input('Enter Post-Tax Yearly Income:\60n$'))
 print(income)
 monthly_income = income/12
 print(monthly_income)
-monthly_minus_taxes = monthly_income * .73
+
+# Want to take monthly income and create ranges based off percentage of income to be spent on rent
+# ranges are <25%, 25-33%, 33-40%, 40%<.
+# names of Low Cost, Medium Cost, High Cost, Extreme.
+low_cost_lower = 0
+low_cost_upper = monthly_income * .25
+med_cost_lower = monthly_income * .25
+med_cost_upper = monthly_income * .33
+high_cost_lower = monthly_income * .33
+high_cost_upper = monthly_income * .40
+extreme_lower = monthly_income * .40
+extreme_upper = monthly_income
+
--- data_join.py
+++ data_join.py
@@ -19,13 +19,29 @@ print(raw)
 rentals_trimmed = raw.drop(columns='2B Price')
 rentals_trimmed = rentals_trimmed.drop(columns='2B M/M%')
 rentals_trimmed = rentals_trimmed.drop(columns='2B Y/Y%')
+#for index, name in rentals_trimmed['City'].iteritems():
+  #  name = name[0:name.find(',')]
+  #  print(name)
+   # rentals_trimmed['City'][index] = name
 print(rentals_trimmed)
 
 # add location data to cities
 uscities = pd.read_csv('uscities.csv',dtype=str)
+uscities['city-state'] = uscities['city_ascii'] + ', ' + uscities['state_id']
+print(uscities)
 uscities.drop(['timezone','ranking','zips','id','source','military','incorporated',
                   'population','density','city_ascii','state_id',
                   'state_id','state_name','county_fips','county_name'], axis=1, inplace=True)
 
-merged_left= pd.merge(left=rentals_trimmed,right=uscities,how='left', left_on='City', right_on='city')
+
+merged_left= pd.merge(left=rentals_trimmed,right=uscities,how='left', left_on='City', right_on='city-state')
 print(merged_left)
+cleaned_data = merged_left.drop(columns='city-state')
+cleaned_data = cleaned_data.drop(columns='city')
+cleaned_data = cleaned_data.drop(columns='Unnamed: 0')
+print(cleaned_data)
+
+cleaned_data.to_csv('City-Affordability-Data.csv')
+
+
+
